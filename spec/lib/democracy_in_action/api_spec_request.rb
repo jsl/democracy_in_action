f714d175a919eaac0b0e77f832a58fa02a2a8e87
Patrice de Muizon <patrice@radicaldesigns.org>
2008-06-24 17:20:08 -0700

    Added test for buildBody.  Also added test for buildBody with key/value pairs where values are an array (set to pending).  Added code to disable API with specs.

--- spec/lib/democracy_in_action/api_spec_request.rb
+++ spec/lib/democracy_in_action/api_spec_request.rb
@@ -2,11 +2,28 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 
 describe DemocracyInAction::API do
   before do
-    @api = DemocracyInAction::API.new( *api_arguments )
+    @api = DemocracyInAction::API.new( *api_arguments ) 
+    #Net::HTTP::Post.stub!(:new).and_return(stub_everything)
   end
-  it "should include password in url" do
-    @api.
-    raise @api.inspect
-    @api.sendRequest('supporter', {"where" => {'Email' => 'test@test.org'}})
+  describe "buildBody" do
+    it "should convert key value pair into string" do
+      body = @api.send(:buildBody, {"key" => "123456"})
+      body.should == "key=123456"
+    end
+    it "should convert multiple key value pairs into string" do
+      body = @api.send(:buildBody, {"key" => "123456", "email" => "test@domain.org"})
+      body.should == "key=123456&email=test%40domain.org"
+    end
+    it "should convert key value pairs that contain arrays into string" do
+      body = @api.send(:buildBody, {"key" => "123456", "names" => ["austin", "patrice", "seth"]})
+      body.should == "names=austin&names=patrice&names=seth&key=123456"
+    end
+  end
+
+  describe "disabling" do
+    it "should provide ability to disable actually posting data to DIA" do
+      DemocracyInAction::API.disable!
+      DemocracyInAction::API.should be_disabled
+    end
   end
 end
