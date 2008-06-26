c442fc3eb3808f86a351746c01809ea2936aa891
Austin Putman <austin@radicaldesigns.org>
2008-06-25 18:51:03 -0700

    tables are a proxy

--- spec/lib/democracy_in_action/api_spec.rb
+++ spec/lib/democracy_in_action/api_spec.rb
@@ -65,4 +65,58 @@ describe DemocracyInAction::API do
     result = @api.process 'supporter', :Email => 'test3@radicaldesigns.org'
     result.should match( /^\d+$/ )
   end
+
+  describe "proxy" do
+    it "specifies name of table in hash based on method called on api" do
+      @api.should_receive(:get)
+      @api.supporter.get
+    end
+    it "should call get with group" do
+      group_proxy = @api.groups
+      @api.should_receive(:get)
+      @api.groups.get
+    end
+    it "should call the api process call when calling supporter.process" do
+      @api.should_receive(:process)
+      @api.supporter.process
+    end
+
+    it "should pass along the table name to the api" do
+      @api.should_receive(:get).with(hash_including(:table => 'supporter'))
+      @api.supporter.get
+    end
+  
+    it "passes along odd methods to the API for handling" do
+      @api.should_receive(:hot_topic)
+      @api.supporter.hot_topic("Flex")
+    end
+
+    it "responds to columns and returns the correct number" do
+      @api.stub!(:sendRequest).and_return(fixture_file_read('supporter_description.xml'))
+      @api.supporter.columns.size.should == 56
+    end
+    it "describe returns the same thing as columns" do
+      @api.stub!(:sendRequest).and_return(fixture_file_read('supporter_description.xml'))
+      @api.supporter.describe.size.should == 56
+    end
+
+    it "counts the records in the table" do
+      @api.stub!(:sendRequest).and_return(fixture_file_read('supporter_by_limit_1.xml'))
+      @api.supporter.count.should == 11466 
+    end
+  end
+
+  describe "count method" do
+    it "counts the records in the table" do
+      @api.stub!(:sendRequest).and_return(fixture_file_read('supporter_by_limit_1.xml'))
+      @api.count(:table => 'supporter').should == 11466 
+    end
+  end
+
+  describe "columns method" do
+    it "should return a description of the given table" do
+      @api.stub!(:sendRequest).and_return(fixture_file_read('supporter_description.xml'))
+      @api.columns(:table => 'supporter').size.should == 56
+    end
+  end
 end
