f1a7e23a33a915c518e5e7e7cf3a9c4b827f1368
Austin Putman <austin@radicaldesigns.org>
2008-06-26 12:38:29 -0700

    converted domains to nodes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,15 +14,15 @@ Spec::Runner.configure do |config|
     user = ENV['DIA_USER'] || 'dummy'
     pass = ENV['DIA_PASS'] || 'secret'
     org = ENV['DIA_ORG'] || 111
-    domain = ENV['DIA_DOMAIN'] || :salsa
-    {:username => user, :password => pass, :orgkey => org, :domain => domain}
+    node = ENV['DIA_NODE'] || :salsa
+    {:username => user, :password => pass, :orgkey => org, :node => node }
 	end
 	def working_api_arguments
     user = ENV['DIA_USER'] || 'demo'
     pass = ENV['DIA_PASS'] || 'demo'
     org = ENV['DIA_ORG'] || 962 
-    domain = ENV['DIA_DOMAIN'] || :sandbox
-    {:username => user, :password => pass, :orgkey => org, :domain => domain}
+    node = ENV['DIA_DOMAIN'] || :sandbox
+    {:username => user, :password => pass, :orgkey => org, :node => node }
 	end
 
   def fixture_file_read(filename)
