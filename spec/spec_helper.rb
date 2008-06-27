969894cdbefb5413f205d5ef587b9fe43cb8aae9
Seth Walker <seth@radicaldesigns.org>
2008-06-27 11:54:27 -0700

    add back spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,34 @@
+begin
+  require 'spec'
+rescue LoadError
+  require 'rubygems'
+  gem 'rspec'
+  require 'spec'
+end
+
+$:.unshift(File.dirname(__FILE__) + '/../lib')
+require 'democracy_in_action'
+DIA_ENABLED = true
+Spec::Runner.configure do |config|
+	def api_arguments
+    user ||= ENV['USER']
+    pass ||= ENV['PASS']
+    org ||= ENV['ORG']
+		[{ 'authCodes' => [user, pass, org] }]
+	end
+	def working_api_arguments
+    user = 'xxx'
+    pass = 'xxx'
+    org =123 
+		[{ 'authCodes' => [user, pass, org] }]
+	end
+
+	def stub_responses( api )
+		unless api.connected?	
+			response = stub(:body => File.read(File.dirname(__FILE__) + '/fixtures/supporter_by_limit_1.xml'), :get_fields => false, :set_body_internal => false )
+			Net::HTTP::Get.stub!(:new).and_return(response)
+      response = stub(:body => File.read(File.dirname(__FILE__) + '/fixtures/process.xml'), :get_fields => false, :set_body_internal => false )
+			Net::HTTP::Post.stub!(:new).and_return(response)
+		end
+	end
+end
